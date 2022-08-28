1b27620aa139f79442474f154c0ad8ae43249705
brianleroux <b@brian.io>
2022-08-28 09:29:20 -0700

    ok lets seeeee2

--- index.mjs
+++ index.mjs
@@ -4,9 +4,10 @@ import { join } from 'path'
 
 /** helper to return files from basePath */
 export default function getFiles (basePath, folder) {
-  let filesOnly = f => f.split(/\/|\\/).pop().includes('.')
   let expr = join(basePath, folder) + '/**'
   let raw = glob.sync(expr, { dot: false })
+  console.log({ expr, raw })
+  let filesOnly = f => f.split(/\/|\\/).pop().includes('.')
   let cache = raw.filter(filesOnly)
   let base = join(basePath, folder)
   let requestedFolder = f => f.startsWith(base)
